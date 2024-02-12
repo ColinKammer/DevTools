5114dfdec19b5630f767c6624064544540abc117
ColinKammer <colin.kammer@gmx.de>
2024-02-12 21:24:22 +0100

    Fix python virtualenv cmake target for multiple requirements

--- cmake/python.cmake
+++ cmake/python.cmake
@@ -9,12 +9,12 @@ endif()
 
 message("Using System_Python3_EXECUTABLE " ${System_Python3_EXECUTABLE})
 
-macro(add_python_virtualenv_target TargetName Requirements)
+macro(add_python_virtualenv_target TargetName) #additional args are treated as pip requirements
     set(${TargetName}_EXECUTABLE ${CMAKE_BINARY_DIR}/${TargetName}/bin/python3)
 
     add_custom_target(${TargetName}
         COMMAND ${System_Python3_EXECUTABLE} -m virtualenv ${CMAKE_BINARY_DIR}/${TargetName}
-        COMMAND ${${TargetName}_EXECUTABLE} -m pip install --require-virtualenv ${Requirements}
+        COMMAND ${${TargetName}_EXECUTABLE} -m pip install --require-virtualenv ${ARGN}
         COMMAND echo "Python virtual env completed at ${CMAKE_BINARY_DIR}/${TargetName}"
     )
 endmacro()
